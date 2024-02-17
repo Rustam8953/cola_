dd3f67d44152042ebd2d4283ebe672e0da386406
GachiMan <147622462+RustamAlef@users.noreply.github.com>
2024-02-18 04:12:53 +0700

    Upload change

---
+++ catalog/index.php
@@ -0,0 +1,48 @@
+<div class="page">
+    <div class="page-box box">
+        <h1 class="page-title">Каталог</h1>
+        <div class="page-wrap">
+            <ul class="page-list">
+                <li class="page-list__name">
+                    <a href="">
+                        Газированные напитки
+                        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li class="page-list__name">
+                    <a href="">
+                        Газированные напитки
+                        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li class="page-list__name">
+                    <a href="">
+                        Газированные напитки
+                        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li class="page-list__name">
+                    <a href="">
+                        Газированные напитки
+                        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li class="page-list__name">
+                    <a href="">
+                        Газированные напитки
+                        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                    </a>
+                </li>
+                <li class="page-list__name">
+                    <a href="">
+                        Газированные напитки
+                        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+                    </a>
+                </li>
+            </ul>
+            <div class="page-content">
+
+            </div>
+        </div>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.css" integrity="sha512-rd0qOHVMOcez6pLWPVFIv7EfSdGKLt+eafXh4RO/12Fgr41hDQxfGvoi1Vy55QIVcQEujUE1LQrATCLl2Fs+ag==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./assets/style/style.css">
+    <link rel="stylesheet" href="./assets/style/banner.css">
+</head>
+<body>
+    <header class="header"></header>
+    <main class="main">
+        <div class="main-wrap">
+            <section class="banner">
+                <div class="banner-box">
+                    <div class="banner-content">
+    
+                    </div>
+                </div>
+            </section>
+            <section class="col-3">
+                <div class="col-3-box box">
+                    <div class="col-3-content">
+                        <h3 class="section-title">О компании</h3>
+                        <div class="col-3-wrap">
+                            <div class="col-3-l">
+                                <div class="col-3-img">
+                                    <img src="" alt="">
+                                </div>
+                            </div>
+                            <div class="col-3-r">
+                                <p>Производственное объединение «Запсибкола» основано в мае 1997 г. в Новосибирске. За 20 лет мы прошли путь от небольшой торгово-производственной компании компании до крупного завода с развитой региональной сетью продаж.</p>
+                                <p>Сегодня продукцию «Запсибколы» можно приобрести практически в каждой торговой точке Сибири: торговые сети, независимые розничные магазины, придомовые торговые ряды, киоски. Нашими партнерами являются «Ашан», «Холидей», «Лента», «Горожанка», «Мария-Ра» и другие ведущие организации сетевого ритейла.</p>    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="product">
+                <div class="product-box box">
+                    <div class="product-content">
+                        <h2 class="section-title">Наша продукция</h2>
+                        <div class="product-items">
+                            <div class="product-item">
+                                <a href="/" class="product-img">
+                                    <img src="./assets/img/logo/860af24458c5571e42da41e157eadf59.png" alt="">
+                                </a>
+                                <div class="product-info">
+                                    <a href="/" class="product-name">Газированные напитки «Фрутто»</a>
+                                    <p>
+                                        Выпускаются на протяжении 20 лет. Эти лимонады завоевали популярность благодаря яркому, бодрящему вкусу и привлекательной цене.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="product-item">
+                                <a href="/" class="product-img">
+                                    <img src="./assets/img/logo/860af24458c5571e42da41e157eadf59.png" alt="">
+                                </a>
+                                <div class="product-info">
+                                    <a href="/" class="product-name">Газированные напитки «Фрутто»</a>
+                                    <p>
+                                        Выпускаются на протяжении 20 лет. Эти лимонады завоевали популярность благодаря яркому, бодрящему вкусу и привлекательной цене.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="product-item">
+                                <a href="/" class="product-img">
+                                    <img src="./assets/img/logo/860af24458c5571e42da41e157eadf59.png" alt="">
+                                </a>
+                                <div class="product-info">
+                                    <a href="/" class="product-name">Газированные напитки «Фрутто»</a>
+                                    <p>
+                                        Выпускаются на протяжении 20 лет. Эти лимонады завоевали популярность благодаря яркому, бодрящему вкусу и привлекательной цене.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="product-item">
+                                <a href="/" class="product-img">
+                                    <img src="./assets/img/logo/860af24458c5571e42da41e157eadf59.png" alt="">
+                                </a>
+                                <div class="product-info">
+                                    <a href="/" class="product-name">Газированные напитки «Фрутто»</a>
+                                    <p>
+                                        Выпускаются на протяжении 20 лет. Эти лимонады завоевали популярность благодаря яркому, бодрящему вкусу и привлекательной цене.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="product-item">
+                                <a href="/" class="product-img">
+                                    <img src="./assets/img/logo/860af24458c5571e42da41e157eadf59.png" alt="">
+                                </a>
+                                <div class="product-info">
+                                    <a href="/" class="product-name">Газированные напитки «Фрутто»</a>
+                                    <p>
+                                        Выпускаются на протяжении 20 лет. Эти лимонады завоевали популярность благодаря яркому, бодрящему вкусу и привлекательной цене.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="col-1">
+                <div class="col-1-box__box box">
+                    <div class="col-1-content">
+                        <h3 class="section-title">Наши товары</h3>
+                        <div class="col-1-box">
+                            <div class="col-1-items swiper">
+                                <div class="col-1-wrapper swiper-wrapper">
+                                    <div class="col-1-item swiper-slide" id="1">
+                                        <a href="" class="col-1-item__img">
+                                            <img src="./assets/img/product/dc73063978e68480333f7fd85f3c445d.png" alt="">
+                                        </a>
+                                        <a href="" class="col-1-item__name">Фруктовый микс</a>
+                                        <p class="col-1-item__description">
+                                            Напитки «Приятный день» с содержанием сока
+                                        </p>
+                                    </div>
+                                    <div class="col-1-item swiper-slide" id="2">
+                                        <a href="" class="col-1-item__img">
+                                            <img src="./assets/img/product/dc73063978e68480333f7fd85f3c445d.png" alt="">
+                                        </a>
+                                        <a href="" class="col-1-item__name">Фруктовый микс</a>
+                                        <p class="col-1-item__description">
+                                            Напитки «Приятный день» с содержанием сока
+                                        </p>
+                                    </div>
+                                    <div class="col-1-item swiper-slide" id="3">
+                                        <a href="" class="col-1-item__img">
+                                            <img src="./assets/img/product/dc73063978e68480333f7fd85f3c445d.png" alt="">
+                                        </a>
+                                        <a href="" class="col-1-item__name">Фруктовый микс</a>
+                                        <p class="col-1-item__description">
+                                            Напитки «Приятный день» с содержанием сока
+                                        </p>
+                                    </div>
+                                    <div class="col-1-item swiper-slide" id="4">
+                                        <a href="" class="col-1-item__img">
+                                            <img src="./assets/img/product/dc73063978e68480333f7fd85f3c445d.png" alt="">
+                                        </a>
+                                        <a href="" class="col-1-item__name">Фруктовый микс</a>
+                                        <p class="col-1-item__description">
+                                            Напитки «Приятный день» с содержанием сока
+                                        </p>
+                                    </div>
+                                    <div class="col-1-item swiper-slide" id="5">
+                                        <a href="" class="col-1-item__img">
+                                            <img src="./assets/img/product/dc73063978e68480333f7fd85f3c445d.png" alt="">
+                                        </a>
+                                        <a href="" class="col-1-item__name">Фруктовый микс</a>
+                                        <p class="col-1-item__description">
+                                            Напитки «Приятный день» с содержанием сока
+                                        </p>
+                                    </div>
+                                    <div class="col-1-item swiper-slide" id="6">
+                                        <a href="" class="col-1-item__img">
+                                            <img src="./assets/img/product/dc73063978e68480333f7fd85f3c445d.png" alt="">
+                                        </a>
+                                        <a href="" class="col-1-item__name">Фруктовый микс</a>
+                                        <p class="col-1-item__description">
+                                            Напитки «Приятный день» с содержанием сока
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-1-next swiper-button-next"></div>
+                            <div class="col-1-prev swiper-button-prev"></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="col-2">
+                <div class="col-2-box box">
+                    <div class="col-2-content">
+                        <div class="col-2-l">
+                            <p class="col-2-title">Консультация по подбору товаров</p>
+                            <p class="col-2-text">Получите индивидуальное коммерческое предложение, оставиви заявку на консультацию</p>
+                        </div>
+                        <form onsubmit="return false;" class="form">
+                            <div class="form-box">
+                                <div class="form-item">
+                                    <input type="text" name="username" id="username" autocomplete="none" placeholder=" ">
+                                    <span>Имя</span>
+                                </div>
+                                <div class="form-item">
+                                    <input type="text" name="tel" id="tel" autocomplete="none" placeholder=" ">
+                                    <span>Телефон</span>
+                                </div>
+                                <div class="form-item">
+                                    <input type="text" name="mail" id="mail" autocomplete="none" placeholder=" ">
+                                    <span>Email</span>
+                                </div>
+                            </div>
+                            <div class="form-wrap">
+                                <div class="form-item">
+                                    <textarea type="text" name="description" id="description" placeholder=" "></textarea>
+                                    <span>Сообщение</span>
+                                </div>
+                            </div>
+                            <button type="submit" mes-form>задать вопрос</button>
+                        </form>
+                    </div>
+                </div>
+            </section>
+            <div class="arrow">
+                <i class="fa fa-arrow-up" aria-hidden="true"></i>
+            </div>
+        </div>
+    </main>
+    <footer class="footer"></footer>
+    <script src="./component/header/index.js"></script>
+    <script src="./component/footer/index.js"></script>
+    <script src="https://kit.fontawesome.com/f84af15626.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.js" integrity="sha512-Ysw1DcK1P+uYLqprEAzNQJP+J4hTx4t/3X2nbVwszao8wD+9afLjBQYjz7Uk4ADP+Er++mJoScI42ueGtQOzEA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="./assets/script/index.js"></script>
+    <script>
+        new Swiper('.col-1-items', {
+            slidesPerView: 4,
+            spaceBetween: 4,
+            loop: true,
+            navigation: {
+                nextEl: '.col-1-next',
+                prevEl: '.col-1-prev'
+            }
+        })
+    </script>
+</body>
+</html>
